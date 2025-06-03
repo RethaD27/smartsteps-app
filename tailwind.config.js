22fa7dfb4c189258b4b465840850cbf59a46b561
Rethabile Diale <rethadiale27@gmail.com>
2025-06-03 12:23:37 +0200

    Update index.css and tailwind.config.js to include Tailwind CSS directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: ["./src/**/*.{js,jsx}"],
+theme: {
+  extend: {},
+},
+plugins: [],
+
 }
 
